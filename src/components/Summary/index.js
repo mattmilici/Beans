fde89b83c7d4e9f25ba20a2622355c103e1326a4
Matthew Milici <milici.m915@gmail.com>
2021-01-14 20:28:00 -0800

    changes

---
+++ src/components/Summary/index.js
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+
+function Summary() {
+	return (
+		<div>
+			<h1>boob</h1>
+		</div>
+	);
+}
+
+export default Summary;
--- src/components/TinderCard/index.js
+++ src/components/TinderCard/index.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 // import TinderCard from '../react-tinder-card/index'
 import TinderCard from "react-tinder-card";
+import Summary from "../Summary/index.js";
 
 import WorkImg from "./images/work.jpg";
 import EatImg from "./images/eat.jpg";
@@ -40,12 +41,15 @@ function TinderApp() {
 	const characters = db;
 	const [lastDirection, setLastDirection] = useState();
 	const [lastName, setLastName] = useState();
+	const [finalCard, setfinalCard] = useState(false);
 
 	const swiped = (direction, nameToDelete) => {
 		console.log("removing: " + nameToDelete);
 		setLastDirection(direction);
-		// setLastName(character.name);
 		setLastName(nameToDelete);
+		if (nameToDelete === "Overall") {
+			setfinalCard(true);
+		}
 	};
 
 	const outOfFrame = (name) => {
@@ -54,39 +58,45 @@ function TinderApp() {
 
 	return (
 		<div>
-			<link
-				href="https://fonts.googleapis.com/css?family=Damion&display=swap"
-				rel="stylesheet"
-			/>
-			<link
-				href="https://fonts.googleapis.com/css?family=Alatsi&display=swap"
-				rel="stylesheet"
-			/>
-			<h1 className="text-black text-4xl mb-6">Daily Beans</h1>
-			<div className="cardContainer w-11/12 md:w-6/12 h-64 flex items-center justify-center m-auto">
-				{characters.map((character) => (
-					<TinderCard
-						className="swipe absolute flex items-center justify-center w-10/12 md:w-64 h-64 text-white text-lg shadow-4xl"
-						shadow-xl
-						key={character.name}
-						onSwipe={(dir) => swiped(dir, character.name)}
-						onCardLeftScreen={() => outOfFrame(character.name)}
-					>
-						<div
-							style={{ backgroundImage: "url(" + character.url + ")" }}
-							className="relative rounded-lg bg-center bg-cover flex items-center justify-center w-10/12 md:w-64 h-64 "
-						>
-							<h3 className="text-3xl">{character.name}</h3>
-						</div>
-					</TinderCard>
-				))}
-			</div>
-			{lastDirection ? (
-				<h2 className="infoText w-full h-5 flex justify-center flex">
-					You swiped {lastDirection} for {lastName}
-				</h2>
+			{finalCard ? (
+				<Summary />
 			) : (
-				<h2 className="infoText w-full h-5 flex justify-center flex" />
+				<div>
+					<link
+						href="https://fonts.googleapis.com/css?family=Damion&display=swap"
+						rel="stylesheet"
+					/>
+					<link
+						href="https://fonts.googleapis.com/css?family=Alatsi&display=swap"
+						rel="stylesheet"
+					/>
+					<h1 className="text-black text-4xl mb-6">Daily Beans</h1>
+					<div className="cardContainer w-11/12 md:w-6/12 h-64 flex items-center justify-center m-auto">
+						{characters.map((character) => (
+							<TinderCard
+								className="swipe absolute flex items-center justify-center w-10/12 md:w-64 h-64 text-white text-lg shadow-4xl"
+								shadow-xl
+								key={character.name}
+								onSwipe={(dir) => swiped(dir, character.name)}
+								onCardLeftScreen={() => outOfFrame(character.name)}
+							>
+								<div
+									style={{ backgroundImage: "url(" + character.url + ")" }}
+									className="relative rounded-lg bg-center bg-cover flex items-center justify-center w-10/12 md:w-64 h-64 "
+								>
+									<h3 className="text-3xl">{character.name}</h3>
+								</div>
+							</TinderCard>
+						))}
+					</div>
+					{lastDirection ? (
+						<h2 className="infoText w-full h-5 flex justify-center flex">
+							You swiped {lastDirection} for {lastName}
+						</h2>
+					) : (
+						<h2 className="infoText w-full h-5 flex justify-center flex" />
+					)}
+				</div>
 			)}
 		</div>
 	);
